ad22a002a90d2e56ce44e37f1ffac1e8df840ad7
daniloqueirooz <danilodequeirozq@gmail.com>
2022-09-20 23:22:05 -0300

    created a splash screen with the team

--- app/src/main/java/com/example/appgorjeta/SplashScreen.kt
+++ app/src/main/java/com/example/appgorjeta/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.example.appgorjeta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
+
+
+    // Criação da SplashScren - duração: 2s para navegação entre as telas.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
